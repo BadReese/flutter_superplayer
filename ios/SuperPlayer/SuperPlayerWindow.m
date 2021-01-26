e8464c52540aec5d5d8e72e7bc6960f6de8260af
BadReese <BadReese@icloud.com>
2021-01-26 15:29:47 +0800

    隐藏返回按键

--- ios/SuperPlayer/SuperPlayerWindow.m
+++ ios/SuperPlayer/SuperPlayerWindow.m
@@ -83,9 +83,8 @@
     }
     
     [self.superPlayer.controlView fadeOut:0.01];
-    [self.superPlayer.controlView.backBtn setHidden:YES];
     
-    [_rootView bringSubviewToFront:_backBtn];
+    // [_rootView bringSubviewToFront:_backBtn];
     [_rootView bringSubviewToFront:_closeBtn];
     
     _closeBtn.m_width(42).m_height(42).m_top(0).m_right(0);
