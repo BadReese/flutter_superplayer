5e5800a96fca847a706da8f32f8033df54508570
BadReese <BadReese@icloud.com>
2021-01-26 16:13:58 +0800

    .

--- ios/SuperPlayer/SuperPlayerView.m
+++ ios/SuperPlayer/SuperPlayerView.m
@@ -1204,6 +1204,7 @@ static UISlider * _volumeSlider;
                      shiftPlayback:self.isShiftPlayback
                          isPlaying:self.autoPlay];
     [controlView setTitle:_controlView.title];
+    [controlView.backBtn setHidden:YES];
     [controlView setPointArray:_controlView.pointArray];
 }
 
@@ -1211,7 +1212,6 @@ static UISlider * _volumeSlider;
 {
     if (_controlView == nil) {
         self.controlView = [[SPDefaultControlView alloc] initWithFrame:CGRectZero];
-        [self.controlView.backBtn setHidden:YES];
     }
     return _controlView;
 }
